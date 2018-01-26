2f5de198992605112ff9a32343ec47e2853940b2
KaranGupta97 <karankg1997@gmail.com>
2018-01-26 22:49:32 +0530

    Multiple Linear regression

--- Regression/MultipleLinear.py
+++ Regression/MultipleLinear.py
@@ -1,4 +1,4 @@
-# Importing the libraries
+4# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -37,3 +37,23 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Backward elimination for optimal solution
+import statsmodels.formula.api as sm
+X=np.append(arr=np.ones((50,1)).astype(int),values= X ,axis=1)
+X_opt=X[:,[0,1,2,3,4,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+X_opt=X[:,[0,1,3,4,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+X_opt=X[:,[0,3,4,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+X_opt=X[:,[0,3,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+X_opt=X[:,[0,3]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
